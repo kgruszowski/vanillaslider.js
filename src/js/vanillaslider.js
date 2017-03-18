c53b83837dce05395fdc1508ce75135d4ca3816c
Kamil Gruszowski <kamil.gruszowski@gmail.com>
2017-03-18 15:22:39 +0100

    init slider

--- src/js/vanillaslider.js
+++ src/js/vanillaslider.js
@@ -1,3 +1,11 @@
-/**
- * Created by kamil on 18.03.17.
- */
+var VanillaSlider = function(elements) {
+    if (!(this instanceof VanillaSlider)) {
+        return new VanillaSlider(elements);
+    }
+
+    this.elements = elements;
+};
+
+VanillaSlider.prototype.init = function () {
+    console.log(this.elements);
+}
